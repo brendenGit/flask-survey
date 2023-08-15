e735bd7428f3fa6566492089cb707a6eaa796ae3
brenden <brendenjarias@gmail.com>
2023-08-15 12:20:07 -0700

    created new branch (using_sessions) for updated code using session in flask

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
+
 app = Flask(__name__)
+
 
 app.config['SECRET_KEY'] = "debug123"
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
-question_count = 0
 
 
 @app.route('/')
@@ -17,18 +17,24 @@ def home():
                            survey_instructions=satisfaction_survey.instructions)
 
 
+@app.route('/store-answers')
+def update_answers():
+    session['answers'] = []
+    session['question_count'] = 0
+    return redirect(f"/question/{session['question_count']}")
+
+
 @app.route('/question/<int:q>')
 def show_question(q):
-    global question_count
-    if question_count >= len(satisfaction_survey.questions):
+    if session['question_count'] >= len(satisfaction_survey.questions):
         return redirect('/thank-you')
-    if q == question_count:
+    if q == session['question_count']:
         question = satisfaction_survey.questions[q].question
         choices = satisfaction_survey.questions[q].choices
         return render_template('question.html', question=question, choices=choices)
     else:
         flash("Invalid question order - please complete this question first!")
-        return redirect(f'/question/{question_count}')
+        return redirect(f"/question/{session['question_count']}")
 
 
 @app.route('/thank-you')
@@ -38,10 +44,6 @@ def thank_you():
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    global question_count
-    question_count += 1
-    responses.append(request.form['choice'])
-    return redirect(f'/question/{question_count}')
-
-
-responses = []
+    session['question_count'] += 1
+    session['answers'].append(request.form['choice'])
+    return redirect(f"/question/{session['question_count']}")
